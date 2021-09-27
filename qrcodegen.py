47e7f4939075917b5ab1643e2cb88d9d0a89e00c
git_hit <nikhilpd.99@gmail.com>
2021-09-27 22:31:03 +0530

    Update qrcodegen.py

--- qrcodegen.py
+++ qrcodegen.py
@@ -1,7 +1,7 @@
 import qrcode
 import image
 qr=qrcode.QRCode(
-    version=4, # 15 mean sthe version of the qr code ,  high the no bigger the code image and compplicated picture
+    version=4, # 15 refers to the version of the qr code ,  higher the no bigger the code image and complicated picture
     box_size=4, #size of the b0x where qr code will be displayed
     border=5 #it is the white part of image -- border in all 4 sides with white color
 )
