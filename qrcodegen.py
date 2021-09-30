6a5375b98fb4c628288901d63ea6937ccee34f84
git_hit <nikhilpd.99@gmail.com>
2021-09-30 08:14:59 +0530

    Update qrcodegen.py

--- qrcodegen.py
+++ qrcodegen.py
@@ -1,9 +1,9 @@
 import qrcode
 import image
 qr=qrcode.QRCode(
-    version=4, # 15 refers to the version of the qr code ,  higher the no bigger the code image and complicated picture
-    box_size=4, #size of the b0x where qr code will be displayed
-    border=5 #it is the white part of image -- border in all 4 sides with white color
+    version=4, # 4 refers to the version of the qr code ,  higher the no bigger the code image and complicated picture
+    box_size=4, #size of the box where qr code will be displayed
+    border=5 # outline of image -- border in all 4 sides 
 )
 
 data="https://www.linkedin.com/in/nikhil-prasad-09716218a/" #any website address
